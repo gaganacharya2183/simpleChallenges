7f1e18e597bbf3094f08bf9587d22eab5972fa07
GaganAcharya2183 <gaganacharya@outlook.com>
2022-02-20 14:12:54 -0800

    LeapYearCalculator

---
+++ src/leapYearCalculator/Main.java
@@ -0,0 +1,9 @@
+package leapYearCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean checkLeapYear= LeapYearCalculator.isLeapYear(1800);
+        System.out.println(checkLeapYear);
+    }
+
+}
