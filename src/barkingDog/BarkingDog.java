2a6b512c537537585ab2f438b7941f7603f99a43
GaganAcharya2183 <gaganacharya@outlook.com>
2022-02-20 13:21:10 -0800

    barkingDog

--- src/barkingDog/BarkingDog.java
+++ src/barkingDog/BarkingDog.java
@@ -1,4 +1,21 @@
 package barkingDog;
 
 public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+//         if(!barking || hourOfDay < 0 || hourOfDay > 23 || (hourOfDay > 7 && hourOfDay < 23)){
+//                return false;
+//            }
+//                return true;
+        if(hourOfDay<0 || hourOfDay>23){
+            return false;
+
+        }else if(barking && (hourOfDay<8 || hourOfDay>22)){
+            return true;
+
+        }else if(!barking){
+            return false;
+        } else {
+            return false;
+        }
+    }
 }
--- src/barkingDog/Main.java
+++ src/barkingDog/Main.java
@@ -1,4 +1,10 @@
 package barkingDog;
 
 public class Main {
+    public static void main(String[] args) {
+       boolean bark= BarkingDog.shouldWakeUp(true,7);
+        System.out.println(bark);
+    }
+
+
 }
